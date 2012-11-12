444355710c3a9b5f8e889c9f628d76d091c0d656
Igor Tripolskiy <itripolskiy@ebay.com>
2012-11-12 17:38:30 +0200

    Implemented: map categories data container

---
+++ js/data.js
@@ -0,0 +1,433 @@
+app.map_categories = {
+    '10': { "id": 10,
+        "title": 'Авто',
+        "subcategories": [
+
+            { "id": 65,
+                "title": 'Автозапчасти',
+                "obj_count": 0
+            },
+
+            { "id": 64,
+                "title": 'Автомойки',
+                "obj_count": 0
+            },
+
+            { "id": 24,
+                "title": 'Автосалоны',
+                "obj_count": 0
+            },
+
+            { "id": 14,
+                "title": 'Автосервисы',
+                "obj_count": 0
+            },
+
+            { "id": 26,
+                "title": 'Автошколы',
+                "obj_count": 0
+            },
+
+            { "id": 23,
+                "title": 'АЗС',
+                "obj_count": 0
+            },
+
+            { "id": 25,
+                "title": 'Стоянки и гаражи',
+                "obj_count": 0
+            },
+
+            { "id": 63,
+                "title": 'Шиномонтажи',
+                "obj_count": 0
+            }
+
+        ]
+    },
+
+    '83': { "id": 83,
+        "title": 'Животные',
+        "subcategories": [
+
+            { "id": 86,
+                "title": 'Вет.учреждения',
+                "obj_count": 0
+            },
+
+            { "id": 84,
+                "title": 'Зоомагазины',
+                "obj_count": 0
+            }
+
+        ]
+    },
+
+    '39': { "id": 39,
+        "title": 'Здоровье и красота',
+        "subcategories": [
+
+            { "id": 43,
+                "title": 'Аптеки',
+                "obj_count": 2
+            },
+
+            { "id": 42,
+                "title": 'Косметика и парфюмерия',
+                "obj_count": 1
+            },
+
+            { "id": 45,
+                "title": 'Мед.учреждения',
+                "obj_count": 0
+            },
+
+            { "id": 99,
+                "title": 'Оптика',
+                "obj_count": 1
+            },
+
+            { "id": 40,
+                "title": 'Парикмахерские',
+                "obj_count": 2
+            },
+
+            { "id": 41,
+                "title": 'Салоны красоты',
+                "obj_count": 2
+            },
+
+            { "id": 44,
+                "title": 'Стоматологии',
+                "obj_count": 1
+            }
+
+        ]
+    },
+
+    '46': { "id": 46,
+        "title": 'Культура',
+        "subcategories": [
+
+            { "id": 98,
+                "title": 'Библиотеки',
+                "obj_count": 0
+            },
+
+            { "id": 49,
+                "title": 'Музеи',
+                "obj_count": 0
+            },
+
+            { "id": 48,
+                "title": 'Театр и кино',
+                "obj_count": 0
+            },
+
+            { "id": 47,
+                "title": 'Цервки и храмы',
+                "obj_count": 0
+            }
+
+        ]
+    },
+
+    '35': { "id": 35,
+        "title": 'Образование',
+        "subcategories": [
+
+            { "id": 37,
+                "title": 'ВУЗы и техникумы',
+                "obj_count": 1
+            },
+
+            { "id": 38,
+                "title": 'Детские сады',
+                "obj_count": 0
+            },
+
+            { "id": 100,
+                "title": 'Курсы',
+                "obj_count": 0
+            },
+
+            { "id": 36,
+                "title": 'Школы',
+                "obj_count": 0
+            }
+
+        ]
+    },
+
+    '71': { "id": 71,
+        "title": 'Отдых и туризм',
+        "subcategories": [
+
+            { "id": 79,
+                "title": 'Бани и сауны',
+                "obj_count": 0
+            },
+
+            { "id": 82,
+                "title": 'Боулинг/бильярд',
+                "obj_count": 0
+            },
+
+            { "id": 72,
+                "title": 'Гостиницы',
+                "obj_count": 0
+            },
+
+            { "id": 75,
+                "title": 'Достопримечательности',
+                "obj_count": 0
+            },
+
+            { "id": 80,
+                "title": 'Клубы',
+                "obj_count": 0
+            },
+
+            { "id": 76,
+                "title": 'Музеи',
+                "obj_count": 0
+            },
+
+            { "id": 78,
+                "title": 'Памятники',
+                "obj_count": 0
+            },
+
+            { "id": 77,
+                "title": 'Парки, скверы и аллеи',
+                "obj_count": 0
+            },
+
+            { "id": 74,
+                "title": 'Санатории',
+                "obj_count": 0
+            },
+
+            { "id": 81,
+                "title": 'Театр и кино',
+                "obj_count": 0
+            },
+
+            { "id": 73,
+                "title": 'Турагентства',
+                "obj_count": 0
+            }
+
+        ]
+    },
+
+    '27': { "id": 27,
+        "title": 'Питание',
+        "subcategories": [
+
+            { "id": 22,
+                "title": 'Банкетные залы',
+                "obj_count": 0
+            },
+
+            { "id": 6,
+                "title": 'Бары',
+                "obj_count": 2
+            },
+
+            { "id": 97,
+                "title": 'Доставка',
+                "obj_count": 0
+            },
+
+            { "id": 96,
+                "title": 'Кафе',
+                "obj_count": 0
+            },
+
+            { "id": 18,
+                "title": 'Рестораны',
+                "obj_count": 0
+            },
+
+            { "id": 20,
+                "title": 'Столовые',
+                "obj_count": 3
+            }
+
+        ]
+    },
+
+    '66': { "id": 66,
+        "title": 'Спорт',
+        "subcategories": [
+
+            { "id": 67,
+                "title": 'Бассейны',
+                "obj_count": 0
+            },
+
+            { "id": 69,
+                "title": 'Клубы',
+                "obj_count": 0
+            },
+
+            { "id": 70,
+                "title": 'Спортмагазины',
+                "obj_count": 0
+            },
+
+            { "id": 68,
+                "title": 'Тренажерные залы',
+                "obj_count": 0
+            }
+
+        ]
+    },
+
+    '101': { "id": 101,
+        "title": 'Торговля',
+        "subcategories": [
+
+            { "id": 113,
+                "title": 'Алкоголь и табак',
+                "obj_count": 6
+            },
+
+            { "id": 107,
+                "title": 'Бытовая техника',
+                "obj_count": 1
+            },
+
+            { "id": 109,
+                "title": 'Книги',
+                "obj_count": 1
+            },
+
+            { "id": 108,
+                "title": 'Компьютеры',
+                "obj_count": 0
+            },
+
+            { "id": 110,
+                "title": 'Подарки',
+                "obj_count": 1
+            },
+
+            { "id": 103,
+                "title": 'Продукты питания',
+                "obj_count": 3
+            },
+
+            { "id": 104,
+                "title": 'Рынки',
+                "obj_count": 0
+            },
+
+            { "id": 106,
+                "title": 'Сотовая связь',
+                "obj_count": 1
+            },
+
+            { "id": 114,
+                "title": 'Стройматериалы',
+                "obj_count": 0
+            },
+
+            { "id": 111,
+                "title": 'Товары для детей',
+                "obj_count": 1
+            },
+
+            { "id": 102,
+                "title": 'Торговые центры',
+                "obj_count": 1
+            },
+
+            { "id": 105,
+                "title": 'Цветы',
+                "obj_count": 1
+            },
+
+            { "id": 112,
+                "title": 'Ювелирные',
+                "obj_count": 1
+            }
+
+        ]
+    },
+
+    '87': { "id": 87,
+        "title": 'Услуги',
+        "subcategories": [
+
+            { "id": 92,
+                "title": 'Бытовые услуги',
+                "obj_count": 0
+            },
+
+            { "id": 95,
+                "title": 'Гос.учреждения',
+                "obj_count": 0
+            },
+
+            { "id": 88,
+                "title": 'Недвижимость',
+                "obj_count": 0
+            },
+
+            { "id": 89,
+                "title": 'Нотариусы',
+                "obj_count": 0
+            },
+
+            { "id": 93,
+                "title": 'Ремонт',
+                "obj_count": 0
+            },
+
+            { "id": 90,
+                "title": 'Связь',
+                "obj_count": 0
+            },
+
+            { "id": 94,
+                "title": 'Страхование',
+                "obj_count": 0
+            },
+
+            { "id": 91,
+                "title": 'Юридические услуги',
+                "obj_count": 0
+            }
+
+        ]
+    },
+
+    '29': { "id": 29,
+        "title": 'Финансы',
+        "subcategories": [
+
+            { "id": 30,
+                "title": 'Банки',
+                "obj_count": 2
+            },
+
+            { "id": 50,
+                "title": 'Банкоматы',
+                "obj_count": 2
+            },
+
+            { "id": 31,
+                "title": 'Обмен валют',
+                "obj_count": 2
+            },
+
+            { "id": 32,
+                "title": 'Страховые услуги',
+                "obj_count": 1
+            }
+
+        ]
+    }
+};
